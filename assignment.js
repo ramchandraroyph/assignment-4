4580f0c60fe69c55d9629327df7d587324893adc
ramchandraroyph <ramchandraroyph@gmail.com>
2023-02-08 19:34:26 +0600

    first problem

---
+++ assignment.js
@@ -0,0 +1,13 @@
+// First problem
+
+function mindGame(number){
+const multification=number*3;
+const addition=multification+10;
+const dividation=addition/2;
+const minus=dividation-5;
+let final=minus;
+return final;
+}
+
+const finalResult=mindGame(5);
+console.log(finalResult);
